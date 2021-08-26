85d4576f24c8b247f234b243b6ff6c901aa6d5f7
ametaastha <54626933+ametaastha@users.noreply.github.com>
2021-08-26 18:46:29 +0530

    Add files via upload

---
+++ PhotoGrid/PhotoGrid/View/ImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCollectionViewCell.swift
+//  PhotoGrid
+//
+//  Created by Astha Ameta on 8/19/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var gridImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
